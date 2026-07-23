5afee677997f96f16fcce840b0f8d1e63d50912b
Grigoriy Pridatkin <pridatkin@gmail.com>
2026-07-24 01:06:02 +0300

    Add textfield for xyz data

--- molecule.js
+++ molecule.js
@@ -1,15 +1,11 @@
-class Position {
-  constructor(x = 0, y = 0, z = 0) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-}
-
 class Atom {
   constructor(element, x = 0, y = 0, z = 0) {
     this.element = element;
-    this.position = new Position(x, y, z);
+    this.position = {
+      x: x,
+      y: y,
+      z: z,
+    };
   }
 }
 
@@ -30,7 +26,7 @@ class Molecule {
     let atom = null;
 
     for (let line of lines) {
-      console.log(line);
+      //console.log(line);
       let data = line.split(/\s+/);
       atom = new Atom(
         data[0],
@@ -42,25 +38,3 @@ class Molecule {
     }
   }
 }
-
-function test() {
-  let test = new Molecule("pyridine");
-  xyzData = `11
-
-C       -0.180226841      0.360945118     -1.120304970
-C       -0.180226841      1.559292118     -0.407860970
-C       -0.180226841      1.503191118      0.986935030
-N       -0.180226841      0.360945118      1.29018350
-C       -0.180226841     -0.781300882      0.986935030
-C       -0.180226841     -0.837401882     -0.407860970
-H       -0.180226841      0.360945118     -2.206546970
-H       -0.180226841      2.517950118     -0.917077970
-H       -0.180226841      2.421289118      1.572099030
-H       -0.180226841     -1.699398882      1.572099030
-H       -0.180226841     -1.796059882     -0.917077970`;
-
-  test.loadFromXYZData(xyzData);
-  console.log(test);
-}
-
-test();
--- script.js
+++ script.js
@@ -1,99 +1,69 @@
-// Три, так называемых, кита Three.js включают в себя:
-// Scene — своеобразная платформа, где размещаются все объекты, которые мы создаем;
-// Camera — по сути — это “глаз”, который будет направлен на сцену. Камера снимает и отображает объекты, которые расположены на сцене;
-// Renderer — визуализатор, который позволяет показывать сцену, снятую камерой.
-
-// Ambient Light — фоновое освещение, которое используется для освещения всех объектов сцены одинаково; не может быть использован для создания теней, так как не имеет направления.
-// Directional Light — свет, который излучается в определенном направлении. Этот свет будет вести себя так, как если бы он был бесконечно далеко, а лучи, излучаемые из него, были параллельны; данное освещение может отбрасывать тени, так как направлено оно на конкретный объект.
-// Point Light — свет, который излучается из одной точки во всех направлениях. Обычный случай использования такого освещения это повторение освещения от простой лампочки (без светильника).
-// Spot Light — данный свет излучается из одной точки в одном направлении, вдоль конуса, расширяемого по мере удаления от источника света.
-
 import * as THREE from "three";
-
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const field = document.querySelector(".sceneField"); //поле для сцены
-const fieldWidth = field.clientWidth;
-const fieldHeight = field.clientHeight;
+const colorsByElement = {
+  H: 0xffffff, // white
+  O: 0xff0000, // red
+  C: 0xc8c8c8, // grey
+  N: 0x8f8fff, // blue
+};
 
-// Добавляем сцену
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+Object.freeze(colorsByElement);
+
+const drawBtn = document.querySelector("#drawBtn");
+const xyzInput = document.querySelector("#xyzInput");
+const sceneFields = document.querySelector("#sceneFields");
+
+drawBtn.addEventListener("click", drawMolecule);
 
 // Добавляем камеру - Perspective Camera (Перспективная камера предназначена для имитации того, что видит человеческий глаз. Камера воспринимает все объекты в перспективной проекции, то есть: чем дальше находится объект от нас, тем он кажется меньше.)
 const fov = 75; //Field Of View (поле/угол зрения) — определяет угол, который вы можете видеть вокруг центра камеры
-const aspectRatio = fieldWidth / fieldHeight; // Пропорция, или, соотношение ширины к высоте экрана. При больших значениях поля зрения видимый размер объектов быстро уменьшается на удалении. При маленьких значениях, наоборот, видимый размер объектов слабо зависит от расстояния.
-const near = 0.1; // Минимальное  и
-const far = 1000; // максимальное расстояние от камеры, которое попадает в рендеринг.
-const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
-// Двигаем камеру по оси z (по умолчанию (0, 0, 0))
-camera.position.set(0, 0, 50);
-camera.lookAt(0, 0, 0);
+const aspectRatio = 1.5; // Пропорция, или, соотношение ширины к высоте экрана. При больших значениях поля зрения видимый размер объектов быстро уменьшается на удалении. При маленьких значениях, наоборот, видимый размер объектов слабо зависит от расстояния.
+const near = 0.01; // Минимальное  и
+const far = 100; // максимальное расстояние от камеры, которое попадает в рендеринг.
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(fieldWidth, fieldHeight);
-field.appendChild(renderer.domElement);
+function drawMolecule() {
+  let molecule = new Molecule();
+  const xyzData = xyzInput.value;
+  molecule.loadFromXYZData(xyzData);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+  const sceneField = document.createElement("div");
+  sceneField.classList.add("sceneField");
+  sceneFields.append(sceneField);
 
-// Mesh — это класс, представляющий объекты на основе треугольной полигональной сетки.
-// Этот класс принимает 2 аргумента:
-// Geometry — описывает форму (положения вершин, грани, радиус и т.д)
-// Material — описывает внешний вид объектов (цвет, текстура, прозрачность и т.д.)
+  const fieldWidth = sceneField.clientWidth;
+  const fieldHeight = sceneField.clientHeight;
 
-// Создаем куб 10x10x10
-const cubeGeometry = new THREE.BoxGeometry(10, 10, 10); //ширина, высота, глубина
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Материал для придания однородного цвета
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.x = 35;
-cube.position.y = -25;
-scene.add(cube);
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color("black");
 
-//Создаем сферу с радиусом 10
-const sphereGeometry = new THREE.SphereGeometry(9, 8, 8); //радиус, количество горизонтальных и вертикальных сегментов
-const sphereMaterial = new THREE.MeshNormalMaterial(); // Многоцветный материал
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.x = -35;
-sphere.position.y = -25;
-scene.add(sphere);
+  const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+  // Двигаем камеру по оси z (по умолчанию (0, 0, 0))
+  camera.position.set(0, 0, 5);
+  camera.lookAt(0, 0, 0);
 
-// Освещение сцены
-// Ambient Light — фоновое освещение, которое используется для освещения всех объектов сцены одинаково; не может быть использован для создания теней, так как не имеет направления.
-// Directional Light — свет, который излучается в определенном направлении. Этот свет будет вести себя так, как если бы он был бесконечно далеко, а лучи, излучаемые из него, были параллельны; данное освещение может отбрасывать тени, так как направлено оно на конкретный объект.
-// Point Light — свет, который излучается из одной точки во всех направлениях. Обычный случай использования такого освещения это повторение освещения от простой лампочки (без светильника).
-// Spot Light — данный свет излучается из одной точки в одном направлении, вдоль конуса, расширяемого по мере удаления от источника света.
-// Добавим свет, нужен для некоторых материалов
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(fieldWidth, fieldHeight);
+  sceneField.appendChild(renderer.domElement);
 
-const spotLight = new THREE.SpotLight(0xffffff, 10000);
-spotLight.target.position.set(0, 0, 0); //Направление куда светит свет
-scene.add(spotLight.target);
-spotLight.position.set(50, 50, 50); // Позиция источника
-scene.add(spotLight);
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-// const ambientLight = new THREE.AmbientLight(0x404040);
-// scene.add(ambientLight);
+  function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+  }
+  render(renderer, scene, camera);
 
-//Создаем тор
-const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100); //Радиус, диаметр трубы, количество сегментов треугольников и граней
-const torusMaterial = new THREE.MeshPhongMaterial({
-  color: 0xdaa520,
-  specular: 0xbcbcbc, // блеск материала
-}); // Материал для блестящих поверхностей
-const torus = new THREE.Mesh(torusGeometry, torusMaterial);
-torus.position.x = 0;
-torus.position.y = 0;
-scene.add(torus);
-
-function render() {
-  requestAnimationFrame(render);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
+  for (let atom of molecule.atoms) {
+    drawSphere(scene, 0.3, atom.position, colorsByElement[atom.element]);
+  }
 }
 
-render();
+function drawSphere(scene, radius, position, color) {
+  const sphereGeometry = new THREE.SphereGeometry(radius, 10, 10);
+  const sphereMaterial = new THREE.MeshBasicMaterial({ color: color });
+
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.set(position.x, position.y, position.z);
+  scene.add(sphere);
+}
